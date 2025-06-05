af5b5c56f0cbebf30a93349fd703c593d42512e9
Young Sun <rladudtjs5678@naver.com>
2025-06-05 10:06:57 +0900

    init

---
+++ client/vite.config.ts
@@ -0,0 +1,50 @@
+import { fileURLToPath, URL } from 'url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vuetify from 'vite-plugin-vuetify';
+
+const server = 'http://localhost:3721/';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => ['v-list-recognize-title'].includes(tag)
+        }
+      }
+    }),
+    vuetify({
+      autoImport: true
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {}
+    }
+  },
+  build: {
+    chunkSizeWarningLimit: 1024 * 1024 // Set the limit to 1 MB
+  },
+  optimizeDeps: {
+    exclude: ['vuetify'],
+    entries: ['./src/**/*.vue']
+  },
+  server : {
+    port : 3722,
+    host: 'localhost',
+    proxy : {
+      '^/api' : {
+        target : server,
+        changeOrigin: true,
+        rewrite : (path) => path.replace(/^/api/, '/')
+      }
+    }
+  }
+});
