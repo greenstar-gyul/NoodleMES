bf165983ec8ff5ce37d4818d8ae9125c9015b116
wnsl1364 <wnsl1364@naver.com>
2025-06-08 23:21:10 +0900

    한주연-주문관리-20250608

---
+++ client/src/service/OrderService.js
@@ -0,0 +1,11 @@
+// src/service/OrderService.js
+
+const orders = [
+    { id: 1, ord_code: 'MES00123', ord_date: '2025.05.26', ord_name: '예담 신라면외 2건', client: '예담', note: '비고입니다1' },
+    { id: 2, ord_code: 'MES00123', ord_date: '2025.05.26', ord_name: '라면이조아 신라면외 1건', client: '라면이조아', note: '비고입니다2'},
+    { id: 3, ord_code: 'MES00124', ord_date: '2025.05.26', ord_name: '카모', client: '카모', note: '비고입니다3'},
+    { id: 4, ord_code: 'MES00125', ord_date: '2025.05.26', ord_name: '지니스라면', client: '지니스라면', note: '비고입니다4'},
+    { id: 5, ord_code: 'MES00126', ord_date: '2025.05.26', ord_name: '라면프레쉬', client: '라면프레쉬', note: '비고입니다5'},
+];
+
+export default orders;
