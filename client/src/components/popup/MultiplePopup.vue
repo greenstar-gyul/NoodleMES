5e2b9471cf58f305395b985105643ea0804ff26d
greenstar-gyul <wasdzxcv@naver.com>
2025-06-09 10:07:43 +0900

    함동의-Update Popup-20250608

--- client/src/components/popup/MultiplePopup.vue
+++ client/src/components/popup/MultiplePopup.vue
@@ -1,5 +1,5 @@
 <template>
-  <Dialog :visible="visible" modal header="주문정보 조회" :style="{ width: '70vw' }" :closable="false">
+  <Dialog :visible="visible" modal :header="title" :style="{ width: '70vw' }" :closable="false">
     <!-- 검색창 -->
     <div class="flex items-center gap-2 mb-4">
       <InputText v-model="searchKeyword" placeholder="주문번호 또는 주문명 또는 거래처를 입력해주세요." class="flex-1" />
@@ -58,6 +58,10 @@ const props = defineProps({
   mapper: {
       type: Array,
       required: true
+  },
+    title: {
+    type: String,
+    default: ''
   }
 });
 const emit = defineEmits(['update:visible', 'confirm']);
