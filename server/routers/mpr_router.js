b521032b3f54e53def41a8b5ecaa6d8a5ec0587c
tmm2176 <studybasic2176@gmail.com>
2025-06-12 17:43:59 +0900

    김민수-자재요청목록기능-250612

--- server/routers/mpr_router.js
+++ server/routers/mpr_router.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
  // 해당 라우터를 통해 제공할 서비스를 가져옴
 const mprService = require('../services/mpr_service.js');
+const { convertObjToAry } = require('../utils/converts.js');
 
 // 라우팅  = 사용자의 요청(URL+METHOD) + Service + 응답형태(View or Data)
 // 실제 라우팅 등록 영역
@@ -13,6 +14,24 @@ router.get('/all', async (req, res)=>{
     // 해당 엔드포인트(URL+METHOD)로 접속할 경우 제공되는 서비스를 실행
     // -> 서비스가 DB에 접속하므로 비동기 작업, await/async를 활용해서 동기식으로 동작하도록 진행
     let mprList = await mprService.findAll()
+                                    .catch(err => console.log(err));
+
+    // res(Http Response에 대응되는 변수)의 응답메소드를 호출해 데이터를 반환하거나 통신을 종료함 
+    // 주의사항) res(Http Response에 대응되는 변수)의 응답메소드를 호출하지 않으면 통신이 종료되지 않음                   
+    // res.send()는 데이터를 반환하는 응답 메소드며 객체로 반환되므로 JSON으로 자동 변환
+    res.send(mprList); 
+});
+
+// 전체조회 : 자원(데이터) -> books / 조회 -> GET
+router.get('/search', async (req, res)=>{
+    // 해당 엔드포인트(URL+METHOD)로 접속할 경우 제공되는 서비스를 실행
+    // -> 서비스가 DB에 접속하므로 비동기 작업, await/async를 활용해서 동기식으로 동작하도록 진행
+    const search = req.query;
+
+    const values = convertObjToAry(search, ['mpr_code','req_date_from','req_date_to','deadline_from','deadline_to','mrp_code','mcode'])
+
+    console.log(values);
+    let mprList = await mprService.findSearch(values)
                                     .catch(err => console.log(err));
 
     // res(Http Response에 대응되는 변수)의 응답메소드를 호출해 데이터를 반환하거나 통신을 종료함 
