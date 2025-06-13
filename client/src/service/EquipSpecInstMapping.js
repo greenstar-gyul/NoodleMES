e9536b99219cc22d06bc9d837a87f1da2ddfbaed
dder2013132 <dder2013@gmail.com>
2025-06-13 17:20:46 +0900

    new_commit_20250613

--- client/src/service/EquipSpecInstMapping.js
+++ client/src/service/EquipSpecInstMapping.js
@@ -1,4 +1,4 @@
-export const eqiiMapping = {
+export const eqsiMapping = {
     eqii_code: '점검지시서코드',
     inst_date: '지시일자',
     eq_chk_type: '점검유형',
@@ -8,4 +8,4 @@ export const eqiiMapping = {
     eq_code: '설비코드'
 };
 
-export default eqiiMapping;
+export default eqsiMapping;
