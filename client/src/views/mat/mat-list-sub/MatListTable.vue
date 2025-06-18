0d07663816598fbd70e3ca736ea2ea01e8b1003f
tmm2176 <studybasic2176@gmail.com>
2025-06-18 17:46:53 +0900

    김민수-ㅇ-250618

---
+++ client/src/views/mat/mat-list-sub/MatListTable.vue
@@ -0,0 +1,23 @@
+<script setup>
+// import axios from 'axios';
+import { onMounted, defineProps, defineEmits } from 'vue';
+import mprMapping from '@/service/MprMapping.js';
+import TableWithExcel from '../../../components/form/TableWithExcel.vue';
+
+// 상위에서 전달받은 데이터
+defineProps({
+  mprdata: Array,
+  required: true,
+});
+
+const emit = defineEmits(['initData']);
+onMounted(() => {
+  emit('initData');
+});
+
+</script>
+
+<template>
+   <!-- 결과 테이블 -->
+  <TableWithExcel :data="mprdata" :dataKey="'mpr_code'" :mapper="mprMapping.MprMapper" title="검색결과" />
+</template>
