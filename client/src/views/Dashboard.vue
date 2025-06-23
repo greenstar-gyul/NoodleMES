2a9ea810fe698283301cbc5b70826d15d72b5c4b
Nugulk <gury126@gmail.com>
2025-06-23 17:03:38 +0900

    메인화면추가

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -1,22 +1,21 @@
 <script setup>
-import BestSellingWidget from '@/components/dashboard/BestSellingWidget.vue';
-import NotificationsWidget from '@/components/dashboard/NotificationsWidget.vue';
-import RecentSalesWidget from '@/components/dashboard/RecentSalesWidget.vue';
-import RevenueStreamWidget from '@/components/dashboard/RevenueStreamWidget.vue';
-import StatsWidget from '@/components/dashboard/StatsWidget.vue';
+
 </script>
 
 <template>
-    <div class="grid grid-cols-12 gap-8">
-        <StatsWidget />
-
-        <div class="col-span-12 xl:col-span-6">
-            <RecentSalesWidget />
-            <BestSellingWidget />
-        </div>
-        <div class="col-span-12 xl:col-span-6">
-            <RevenueStreamWidget />
-            <NotificationsWidget />
-        </div>
-    </div>
+  <div class="image-container">
+    <img src="../assets/images/noodleMes.png" alt="메인이미지" class="main-image" />
+  </div>
 </template>
+
+<style scoped>
+.image-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.main-image {
+  width: 73%;
+}
+</style>
