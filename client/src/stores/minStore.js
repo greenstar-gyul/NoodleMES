32559b60f27d49e05da49aeb7868842b9ebd76bc
tmm2176 <studybasic2176@gmail.com>
2025-06-25 09:51:14 +0900

    김민수-이거최종인가-250625

--- client/src/stores/minStore.js
+++ client/src/stores/minStore.js
@@ -7,6 +7,7 @@ import { defineStore } from 'pinia';
 import { ref } from 'vue';
 import axios from 'axios';
 import moment from 'moment';
+import { minbndMapping } from '@/service/MinMapping.js';
 
 export const useMinStore = defineStore('minStore', () => {
   // min 목록
@@ -42,6 +43,7 @@ export const useMinStore = defineStore('minStore', () => {
     d.setDate(d.getDate() - n); // n일 전 날짜 반환
     return d;
   }
+  
   // 전체 min정보를 불러오는 함수
   async function fetchAllMins() {
     try {
@@ -55,6 +57,9 @@ export const useMinStore = defineStore('minStore', () => {
         ...min,
         inbnd_date: formatDate(min.inbnd_date),
       }));
+      console.log('mins 테스트')
+      console.log(mins)
+      // console.log(mins.value[0].minbnd_code)
     } catch (err) {
       throw err;
     }
@@ -71,7 +76,7 @@ export const useMinStore = defineStore('minStore', () => {
       const res = await axios.get('/api/min/all', { params });
         mins.value = res.data.map(min => ({
         ...min,
-        inbnd_date_date: formatDate(min.inbnd_date),
+        inbnd_date: formatDate(min.inbnd_date),
       }));
     } catch (err) {
       throw err;
@@ -87,7 +92,6 @@ export const useMinStore = defineStore('minStore', () => {
     selMin.inbndDateTo = getToday();
     selMin.supName = '';
     selMin.mName = '';
-    mins.value = []; // 결과 테이블 비우기
   }
 
   // 목록 데이터 저장
@@ -111,6 +115,7 @@ export const useMinStore = defineStore('minStore', () => {
   
   return {
     minRows,
+    mins,
     selectedMin,
     fetchAllMins,
     fetchMinsSearch,
